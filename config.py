8dbe02e9966bb4cf6ec9520541ac5888499cf883
jgomes <jgomes@google.com>
2012-06-29 20:55:05 +0000

    Updated language list, clearer column format

--- config.py
+++ config.py
@@ -1,8 +1,70 @@
 # -*- coding: UTF-8 -*-
-"""
-A list of the supported languages. See list at http://code.google.com/apis/language/translate/v2/using_rest.html#language-params
-"""
-languages = [('af','Afrikaans'), ('sq','Albanian'), ('ar',u'Arabic'), ('be',u'Belarussian'), ('bg',u'Bulgarian'), ('ca',u'Catalan'), ('zh-CN',u'Chinese Simplified'), ('zh-TW',u'Chinese Traditional'), ('hr','Croatian'), ('cs',u'Czech'), ('da','Danish'), ('nl','Dutch'), ('et','Estonian'), ('tl','Filipino'), ('fi','Finnish'), ('fr',u'French'), ('gl','Galician'), ('de','German'), ('el',u'Greek'), ('ht',u'Haitian Creole'), ('iw',u'Hebrew'), ('hi',u'Hindi'), ('hu','Hungarian'), ('is',u'Icelandic'), ('id','Indonesian'), ('ga',u'Irish'), ('it','Italian'), ('ja',u'Japanese'), ('lv',u'Latvian'), ('lt','Lithuanian'), ('mk',u'Macedonian'), ('ms','Malay'), ('mt','Maltese'), ('no','Norwegian'), ('fa',u'Persian'), ('pl','Polish'), ('pt',u'Portugese'), ('ro',u'Romanian'), ('ru',u'Russian'), ('sr',u'Serbian'), ('sk',u'Slovak'), ('sl',u'Slovenian'), ('es',u'Spanish'), ('sw','Swahili'), ('sv','Swedish'), ('th',u'Thai'), ('tr',u'Turkish'), ('uk',u'Ukranian'), ('vi',u'Vietnamese'), ('cy','Welsh'), ('yi',u'Yiddish')]
+
+# List of enabled languages.
+# Complete list of languages supported by the Google Translate API at:
+# https://developers.google.com/translate/v2/using_rest#language-params
+languages = [
+    ('af', u'Afrikaans'),
+    ('sq', u'Albanian'),
+    ('ar', u'Arabic'),
+    ('az', u'Azerbaijani'),
+    ('eu', u'Basque'),
+    ('be', u'Belarusian'),
+    ('bg', u'Bulgarian'),
+    ('ca', u'Catalan'),
+    ('zh-CN', u'Chinese Simplified'),
+    ('zh-TW', u'Chinese Traditional'),
+    ('hr', u'Croatian'),
+    ('cs', u'Czech'),
+    ('da', u'Danish'),
+    ('nl', u'Dutch'),
+    ('eo', u'Esperanto'),
+    ('et', u'Estonian'),
+    ('tl', u'Filipino'),
+    ('fi', u'Finnish'),
+    ('fr', u'French'),
+    ('gl', u'Galician'),
+    ('ka', u'Georgian'),
+    ('de', u'German'),
+    ('el', u'Greek'),
+    ('gu', u'Gujarati'),
+    ('ht', u'Haitian Creole'),
+    ('iw', u'Hebrew'),
+    ('hi', u'Hindi'),
+    ('hu', u'Hungarian'),
+    ('is', u'Icelandic'),
+    ('id', u'Indonesian'),
+    ('ga', u'Irish'),
+    ('it', u'Italian'),
+    ('ja', u'Japanese'),
+    ('ko', u'Korean'),
+    ('la', u'Latin'),
+    ('lv', u'Latvian'),
+    ('lt', u'Lithuanian'),
+    ('mk', u'Macedonian'),
+    ('ms', u'Malay'),
+    ('mt', u'Maltese'),
+    ('no', u'Norwegian'),
+    ('fa', u'Persian'),
+    ('pl', u'Polish'),
+    ('pt', u'Portuguese'),
+    ('ro', u'Romanian'),
+    ('ru', u'Russian'),
+    ('sr', u'Serbian'),
+    ('sk', u'Slovak'),
+    ('sl', u'Slovenian'),
+    ('es', u'Spanish'),
+    ('sw', u'Swahili'),
+    ('sv', u'Swedish'),
+    ('ta', u'Tamil'),
+    ('th', u'Thai'),
+    ('tr', u'Turkish'),
+    ('uk', u'Ukrainian'),
+    ('ur', u'Urdu'),
+    ('vi', u'Vietnamese'),
+    ('cy', u'Welsh'),
+    ('yi', u'Yiddish'),
+]
 
 rtl_langs = ['iw', 'ar', 'fa', 'yi']
 
